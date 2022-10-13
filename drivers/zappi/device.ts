504cc5cf9655b601d11500e9696f44a8dff6601a
André Biseth <andre.biseth@onyxcentersource.com>
2022-10-13 12:34:04 +0000

    Removed setting of settings whithin onSettings

--- drivers/zappi/device.ts
+++ drivers/zappi/device.ts
@@ -702,7 +702,7 @@ export class ZappiDevice extends Device {
       const prevEnergy: number = this.getCapabilityValue('meter_power');
       this.setCapabilityValue('meter_power', prevEnergy + (newSettings.totalEnergyOffset ? newSettings.totalEnergyOffset : 0));
       this._settings.totalEnergyOffset = 0;
-      this.setSettings({ totalEnergyOffset: 0 })
+      // this.setSettings({ totalEnergyOffset: 0 })
     }
   }
 
